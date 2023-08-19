63b680c05bccfc5bb9532f2461679f3f64f4fe92
Lasha Kandashvili <44940174+lkandashvili@users.noreply.github.com>
2023-08-19 17:23:52 +0400

    da

--- src/main/java/org/example/KidsWithTheGreatestNumberOfCandies.java
+++ src/main/java/org/example/KidsWithTheGreatestNumberOfCandies.java
@@ -5,20 +5,32 @@ import java.util.List;
 
 public class KidsWithTheGreatestNumberOfCandies {
 
-    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+    public static void main(String[] args) {
+        int[] candies = {2,3,5,1,3};
+        System.out.println(kidsWithCandies(candies, 3));
+    }
+
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         List<Boolean> out = new ArrayList<>();
         int greatest = candies[0];
-        for (int i=0; i < candies.length -1; i++) {
-            for (int j=1; j < candies.length; j++) {
 
-                if (candies[i] + candies[j] + extraCandies >= greatest) {
-                    out.add(true);
-                } else {
-                    out.add(false);
-                }
+        for (int k : candies) {
+            if (k > greatest) {
+                greatest = k;
             }
         }
 
+        for (int i=0; i < candies.length -1; i++) {
+
+            if (candies[i] + extraCandies >= greatest) {
+                out.add(true);
+            } else {
+                out.add(false);
+            }
+
+        }
+
         return out;
+
     }
 }
