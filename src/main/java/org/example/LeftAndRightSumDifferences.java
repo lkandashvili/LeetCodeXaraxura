a7464fac23c19e1cf364f83de2d37cdadf92d13d
Lasha Kandashvili <44940174+lkandashvili@users.noreply.github.com>
2023-08-20 17:12:52 +0400

    a

--- src/main/java/org/example/LeftAndRightSumDifferences.java
+++ src/main/java/org/example/LeftAndRightSumDifferences.java
@@ -1,2 +1,23 @@
-package org.example;public class LeftAndRightSumDifferences {
+package org.example;
+
+public class LeftAndRightSumDifferences {
+    public int[] leftRightDifference(int[] nums) {
+
+        int[] answer = new int[nums.length];
+
+        if (nums.length > 1) {
+
+            for (int i=0; i < nums.length; i++) {
+                for (int j=0; j < nums.length; j++) {
+                    answer[i] = nums[i];
+                }
+            }
+
+        }else {
+            return new int[0];
+        }
+
+
+        return answer;
+    }
 }
--- src/main/java/org/example/MaximumNumberOfWordsFoundInSentences.java
+++ src/main/java/org/example/MaximumNumberOfWordsFoundInSentences.java
@@ -1,2 +1,36 @@
-package org.example;public class MaximumNumberOfWordsFoundInSentences {
+package org.example;
+
+public class MaximumNumberOfWordsFoundInSentences {
+    public static void main(String[] args) {
+        String[] sentences = {"alice and bob love leetcode", "i think so too", "this is great thanks very much"};
+
+        int count = 0;
+        int cds = 0;
+        for (String c : sentences) {
+            for (char ch : c.toCharArray()) {
+                if (ch == ' ') {
+                    count++;
+                }
+            }
+            if (count > cds) {
+                cds = count;
+            }
+        }
+    }
+    public static int mostWordsFound(String[] sentences) {
+        int count = 0;
+        int cds = 0;
+
+        for (String c : sentences) {
+            for (char ch : c.toCharArray()) {
+                if (ch == ' ') {
+                    count++;
+                }
+            }
+            if (count > cds) {
+                cds = count;
+            }
+        }
+        return cds;
+    }
 }
