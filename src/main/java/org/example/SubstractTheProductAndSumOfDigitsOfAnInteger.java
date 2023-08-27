00338c6eccc91ca066afa00993bfe159e5e41b78
Lasha Kandashvili <44940174+lkandashvili@users.noreply.github.com>
2023-08-27 18:06:47 +0400

    xodamse

--- src/main/java/org/example/SubstractTheProductAndSumOfDigitsOfAnInteger.java
+++ src/main/java/org/example/SubstractTheProductAndSumOfDigitsOfAnInteger.java
@@ -1,13 +1,18 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SubstractTheProductAndSumOfDigitsOfAnInteger {
     public static void main(String[] args) {
 
+//        System.out.println(subtractProductAndSum(234));
         System.out.println(subtractProductAndSum(234));
 
         int n = 234;
 
-        int product = 1;
+        List<Integer> product = new ArrayList<>();
+
         int sum = 0;
         int x = 5;
 
@@ -17,14 +22,19 @@ public class SubstractTheProductAndSumOfDigitsOfAnInteger {
 //        }
     }
         public static int subtractProductAndSum(int n) {
+            List<Integer> productList = new ArrayList<>();
             int product = 1;
             int sum = 0;
-            int x = 1;
 
-            while (n % ((int) Math.pow(10,x)) != 0 && n % ((int) Math.pow(10,x)) < n) {
-                product = (int) (product * n % (Math.pow(10,x)));
-                sum = (int) (sum + n % (Math.pow(10,x)));
-                x++;
+            while (n > 0) {
+                int x = n % 10;
+                productList.add(x);
+                sum = sum + x;
+                n = n / 10;
+            }
+            while (productList.size() != 0) {
+                product = product * productList.get(productList.size() - 1);
+                productList.remove(productList.size() - 1);
             }
             return product - sum;
         }
