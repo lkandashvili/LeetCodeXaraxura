562bb43201ec34385f4d2fc43b1002f524f16d48
Lasha Kandashvili <44940174+lkandashvili@users.noreply.github.com>
2023-08-28 15:05:57 +0400

    xd

--- src/main/java/org/example/Minimum_Sum_Of_Four_Digit_Number_after_Splitting_Digits.java
+++ src/main/java/org/example/Minimum_Sum_Of_Four_Digit_Number_after_Splitting_Digits.java
@@ -1,13 +1,28 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class Minimum_Sum_Of_Four_Digit_Number_after_Splitting_Digits {
+    public static void main(String[] args) {
+        System.out.println(minimumSum(4009));
+    }
     public static int minimumSum(int num) {
         int firstPair = 0;
         int secondPair = 0;
+        List<Integer> list = new ArrayList<>();
 
         while (num > 0) {
-
+            int x = num % 10;
+            list.add(x);
+            num = num / 10;
         }
+        Collections.sort(list);
+
+        firstPair = firstPair + (list.get(0) * 10) + list.get(3);
+        secondPair = secondPair + (list.get(1) * 10) + list.get(2);
 
         return firstPair + secondPair;
     }
