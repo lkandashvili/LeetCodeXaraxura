537bb6755a57e75690dd99029268505e525d3f0b
Lasha Kandashvili <44940174+lkandashvili@users.noreply.github.com>
2023-08-28 14:54:20 +0400

    c

---
+++ src/main/java/org/example/Minimum_Sum_Of_Four_Digit_Number_after_Splitting_Digits.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Minimum_Sum_Of_Four_Digit_Number_after_Splitting_Digits {
+    public static int minimumSum(int num) {
+        int firstPair = 0;
+        int secondPair = 0;
+
+        while (num > 0) {
+
+        }
+
+        return firstPair + secondPair;
+    }
+}
