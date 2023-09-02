89ca51cfa6fc54227024425510e7a72070198fd6
Lasha Kandashvili <44940174+lkandashvili@users.noreply.github.com>
2023-09-02 18:02:20 +0400

    sdsadsadasdgdsvfgdash

--- src/main/java/org/example/Longest_Substring_Without_Repeating_Characters.java
+++ src/main/java/org/example/Longest_Substring_Without_Repeating_Characters.java
@@ -2,30 +2,37 @@ package org.example;
 
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Longest_Substring_Without_Repeating_Characters {
 
     public static void main(String[] args) {
-        List<Character> chars = new ArrayList<>();
-        if (!chars.isEmpty()) {
-            System.out.println(m);
+
+        String s = "pwwkew";
+
+        Set<Character> chars = new HashSet<>();
+        for (Character c : s.toCharArray()) {
+            chars.add(c);
         }
+
+
+        for (char g : chars) {
+            System.out.println(g);
+        }
+
     }
 
     public static int lengthOfLongestSubstring(String s) {
-        int ans = 0;
 
-        List<Character> chars = new ArrayList<>();
+        Set<Character> chars = new HashSet<>();
 
         for(Character c : s.toCharArray()) {
-            if (!chars.isEmpty()) {
-                chars.add(c);
-
-            }
+            chars.add(c);
         }
 
-        return ans;
+        return chars.size();
     }
 
 }
