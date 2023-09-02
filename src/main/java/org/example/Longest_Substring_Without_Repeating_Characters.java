c41f84c00ca0519d761623e4cc62dfdbe10d5821
Lasha Kandashvili <44940174+lkandashvili@users.noreply.github.com>
2023-09-02 19:14:42 +0400

    jj

--- src/main/java/org/example/Longest_Substring_Without_Repeating_Characters.java
+++ src/main/java/org/example/Longest_Substring_Without_Repeating_Characters.java
@@ -7,7 +7,7 @@ public class Longest_Substring_Without_Repeating_Characters {
 
     public static void main(String[] args) {
 
-        String s = "pwwkew";
+        String s = "bbbbb";
         int count = 0;
         int n = 0;
 
@@ -26,23 +26,30 @@ public class Longest_Substring_Without_Repeating_Characters {
     }
 
     public static int lengthOfLongestSubstring(String s) {
-
+        // pwwkew //
         int count = 0;
         int n = 0;
         Map<Character, Integer> map = new HashMap<>();
 
+
         for(Character c : s.toCharArray()) {
 
-            if (map.containsKey(c)) {
-                continue;
+            if (Character.isLetter(c)) {
+
+                if (map.containsKey(c)) {
+                    n = Math.max(n,count);
+                    map.clear();
+                    count = 0;
+                    map.put(c,count);
+                    count++;
+                    continue;
+                }
+
+                map.put(c, count);
+                count ++;
             }
-
-            map.put(c, count);
-            n++;
-            count ++;
         }
-
-        return Math.max(n, count);
+        return n;
     }
 
 }
