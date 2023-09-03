1fc2d4afcf81549efc69ef05c771ac8276b2facd
Lasha Kandashvili <44940174+lkandashvili@users.noreply.github.com>
2023-09-03 18:00:18 +0400

    kide ver davamtavre bliadjbengr'gjegqgrGJPGJQ;

--- src/main/java/org/example/Longest_Substring_Without_Repeating_Characters.java
+++ src/main/java/org/example/Longest_Substring_Without_Repeating_Characters.java
@@ -7,28 +7,30 @@ public class Longest_Substring_Without_Repeating_Characters {
 
     public static void main(String[] args) {
 
-        String s = "bbbbb";
+        String s = "pwwkew";
+        int max = 0;
+        boolean first = true;
+        HashSet<Character> hashSet = new HashSet<>();
+        char[] arr = s.toCharArray();
+
         System.out.println(lengthOfLongestSubstring(s));
     }
 
     public static int lengthOfLongestSubstring(String s) {
         // dvdf //
-        int ans = 0;
+
         int max = 0;
-
+        StringBuilder stringBuilder = new StringBuilder();
         HashSet<Character> hashSet = new HashSet<>();
-        char[] arr = s.toCharArray();
 
-        for (int i=0; i < s.length(); i++) {
-            if (!hashSet.contains(arr[i])) {
-                hashSet.add(arr[i]);
-            }else {
-                if (max < hashSet.size()) {
-                    max = hashSet.size();
-                }
-                i--;
+        for (Character c : s.toCharArray()) {
+            if (hashSet.contains(c)) {
+                max = Math.max(max,hashSet.size());
                 hashSet.clear();
+            }else {
+                max++;
             }
+            hashSet.add(c);
         }
         return max;
     }
