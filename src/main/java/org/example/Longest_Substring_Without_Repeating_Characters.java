0ab8468b15bf9031bbbf941fe5e581e646d34aff
Lasha Kandashvili <44940174+lkandashvili@users.noreply.github.com>
2023-09-03 16:04:31 +0400

    dedavatire

--- src/main/java/org/example/Longest_Substring_Without_Repeating_Characters.java
+++ src/main/java/org/example/Longest_Substring_Without_Repeating_Characters.java
@@ -7,28 +7,30 @@ public class Longest_Substring_Without_Repeating_Characters {
 
     public static void main(String[] args) {
 
-        String s = "dvdf";
+        String s = "pwwkew";
         System.out.println(lengthOfLongestSubstring(s));
     }
 
     public static int lengthOfLongestSubstring(String s) {
         // dvdf //
         int ans = 0;
+        int max = 0;
         char[] arr = s.toCharArray();
 
         for (int i=0; i < s.length(); i++) {
-            for (int j=1; j < s.length(); j++) {
-                if ( j < i) {
-                    continue;
-                }
-                if (arr[i] != arr[j]) {
+            for (int j=1; j < s.length() & j < i; j++) {
+                if (arr[i] == arr[j]) {
+                    if (ans > max) {
+                        max = ans;
+                    }
+                    ans = 0;
+                    break;
+                } else {
                     ans++;
                 }
-                j++;
             }
         }
-
-        return ans;
+        return max;
     }
 
 }
