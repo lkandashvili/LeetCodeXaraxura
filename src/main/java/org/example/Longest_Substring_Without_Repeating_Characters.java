a1eab01cc73755265470772a72c128d8381ea38e
Lasha Kandashvili <44940174+lkandashvili@users.noreply.github.com>
2023-09-10 16:30:09 +0400

    Sdag

--- src/main/java/org/example/Longest_Substring_Without_Repeating_Characters.java
+++ src/main/java/org/example/Longest_Substring_Without_Repeating_Characters.java
@@ -8,16 +8,12 @@ public class Longest_Substring_Without_Repeating_Characters {
     public static void main(String[] args) {
 
         String s = "pwwkew";
-        int max = 0;
-        boolean first = true;
-        HashSet<Character> hashSet = new HashSet<>();
-        char[] arr = s.toCharArray();
+
 
         System.out.println(lengthOfLongestSubstring(s));
     }
 
     public static int lengthOfLongestSubstring(String s) {
-        // dvdf //
 
         int max = 0;
         StringBuilder stringBuilder = new StringBuilder();
